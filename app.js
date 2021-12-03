239e4f0afcd1eaa711684dbad7ed845850b05a95
Koushik <koushikrp26@gmail.com>
2021-12-03 20:18:41 +0530

    mark6 completed

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+var outputDiv = document.querySelector("#txt-output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+	return serverURL + "?text=" + text;
+}
+function errorHandler(error) {
+	console.log("Error occurred,", error);
+	alert("Something wrong with server: try again after some time");
+}
 function clickEventHandler() {
-	outputDiv.innerText = "afgaef " + txtInput.value;
+	let inputText = txtInput.value;
+	fetch(getTranslationURL(inputText))
+		.then((response) => response.json())
+		.then((jsonString) => (outputDiv.value = jsonString.contents.translated))
+		.catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
